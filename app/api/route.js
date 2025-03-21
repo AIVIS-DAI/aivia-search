7e47af0caada62405a1f6648ed8286e57159a263
AIVIA <diasdiner@gmail.com>
2025-03-21 12:09:38 +0500

    Изменения АПИ

--- app/api/route.js
+++ app/api/route.js
@@ -1,11 +1,11 @@
 export async function GET(req) {
   const { searchParams } = new URL(req.url);
-  const baseCurrency = searchParams.get("base") || "USD"; // По умолчанию USD
-  const targetCurrency = searchParams.get("target") || "EUR"; // По умолчанию EUR
+  const baseCurrency = searchParams.get("base") || "USD"; 
+  const targetCurrency = searchParams.get("target") || "KZT"; 
 
   try {
     const response = await fetch(
-      `https://api.exchangerate.host/live?access_key=896ad2494b71b535747e0760d225a517&source=${baseCurrency}&currencies=${targetCurrency}`
+      `https://api.exchangerate.host/convert?access_key=896ad2494b71b535747e0760d225a517&from=${baseCurrency}&to=${targetCurrency}`
     );
 
     const data = await response.json();
@@ -14,7 +14,7 @@ export async function GET(req) {
       throw new Error("Ошибка при получении данных от API");
     }
 
-    return Response.json({ rate: data.quotes[`${baseCurrency}${targetCurrency}`] });
+    return Response.json({ rate: data.info.rate });
   } catch (error) {
     console.error("Ошибка API:", error);
     return Response.json({ results: [] });
